e26a15f46f433cf23ce0e956eb1b392f644e215e
Yu Jin Choe <forworkchoe@gmail.com>
2022-04-19 20:40:15 +0900

    Create new links

--- pages/posts/first-post.tsx
+++ pages/posts/first-post.tsx
@@ -1,3 +1,15 @@
+import React from 'react';
+import Link from "next/link";
+
 export default function Page() {
-  return <h1>First Post</h1>
+  return (
+    <>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </>
+  )
 }
