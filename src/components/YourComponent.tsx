10251ff5da548078ed994e5f6a9a67697443fc79
Yu Jin Choe <forworkchoe@gmail.com>
2022-04-20 23:43:12 +0900

    Create new a component

---
+++ src/components/YourComponent.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Image from 'next/image';
+
+export default function YourComponent() {
+  return (
+    <Image src="/images/profile.jpg" height={144} width={144} alt="Your Name"/>
+  );
+}
