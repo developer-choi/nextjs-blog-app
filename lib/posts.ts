db927107f336b04c88122e0290035ad50d466c47
Yu Jin Choe <forworkchoe@gmail.com>
2022-04-27 22:13:55 +0900

    Add a page with getStaticPaths

--- lib/posts.ts
+++ lib/posts.ts
@@ -41,3 +41,29 @@ export function getSortedPostsData(): Post[] {
     }
   })
 }
+
+export function getPostData(id: string): Post {
+  const fullPath = path.join(postsDirectory, `${id}.md`)
+  const fileContents = fs.readFileSync(fullPath, 'utf8')
+  
+  // Use gray-matter to parse the post metadata section
+  const matterResult = matter(fileContents).data as Omit<Post, 'id'>;
+  
+  // Combine the data with the id
+  return {
+    id,
+    ...matterResult
+  }
+}
+
+export function getAllPostIds() {
+  const fileNames = fs.readdirSync(postsDirectory)
+  
+  return fileNames.map(fileName => {
+    return {
+      params: {
+        id: fileName.replace(/\.md$/, '')
+      }
+    }
+  })
+}
