4f3824814d39650c464da76d51c36eea814db522
Yu Jin Choe <forworkchoe@gmail.com>
2022-04-28 20:32:59 +0900

    Add remark, remark-html packages

--- lib/posts.ts
+++ lib/posts.ts
@@ -1,6 +1,9 @@
-import fs from 'fs'
-import path from 'path'
-import matter from 'gray-matter'
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+import {remark} from 'remark';
+import html from 'remark-html';
+
 
 const postsDirectory = path.join(process.cwd(), 'posts');
 
@@ -8,6 +11,7 @@ export interface Post {
   id: string;
   date: string;
   title: string;
+  contentHtml: string;
 }
 
 export function getSortedPostsData(): Post[] {
@@ -42,17 +46,24 @@ export function getSortedPostsData(): Post[] {
   })
 }
 
-export function getPostData(id: string): Post {
+export async function getPostData(id: string): Promise<Post> {
   const fullPath = path.join(postsDirectory, `${id}.md`)
   const fileContents = fs.readFileSync(fullPath, 'utf8')
   
   // Use gray-matter to parse the post metadata section
-  const matterResult = matter(fileContents).data as Omit<Post, 'id'>;
+  const matterResult = matter(fileContents);
   
-  // Combine the data with the id
+  // Use remark to convert markdown into HTML string
+  const processedContent = await remark()
+    .use(html)
+    .process(matterResult.content)
+  const contentHtml = processedContent.toString()
+  
+  // Combine the data with the id and contentHtml
   return {
     id,
-    ...matterResult
+    contentHtml,
+    ...matterResult.data as Omit<Post, 'id' | 'contentHtml'>
   }
 }
 
